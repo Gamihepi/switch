979361959e1743998c20c4ce40fdb9a468b6613e
Gamihepi <121499922+Gamihepi@users.noreply.github.com>
2023-02-09 18:44:20 +0530

    Add files via upload

---
+++ pswitchmonth.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("Enter the month number=");
+	scanf("%d",&a);
+	switch(a)
+	{
+		case 1:
+			printf("January");
+			break;
+			
+		
+		case 2:
+			printf("Fabruary");
+			break;
+			
+		
+		case 3:
+			printf("March");
+			break;
+			
+		case 4:
+			printf("April");
+			break;
+			
+		case 5:
+			printf("May");
+			break;
+			
+		case 6:
+			printf("June");
+			break;
+			
+		case 7:
+			printf("July");
+			break;
+			
+		case 8:
+			printf("Augest");
+			break;
+			
+		case 9:
+			printf("September");
+			break;
+			
+		case 10:
+			printf("Octomber");
+			break;
+			
+		case 11:
+			printf("November");
+			break;
+			
+		case 12:
+			printf("December");
+			break;
+	
+		default:
+			printf("Month is not valid.");
+	}
+}
